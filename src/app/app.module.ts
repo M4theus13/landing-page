aa51ba08664331b9abf6fd5192706661c70a3c66
Matheus Garcia <matheusgarciajbs@gmail.com>
2023-09-23 02:31:29 -0300

    page one, style, button, scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PageOneComponent } from './components/pages/page-one/page-one.component';
+import { PageTwoComponent } from './components/pages/page-two/page-two.component';
+import { PageThreeComponent } from './components/pages/page-three/page-three.component';
+import { PageFourComponent } from './components/pages/page-four/page-four.component';
+import { PageFiveComponent } from './components/pages/page-five/page-five.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageOneComponent,
+    PageTwoComponent,
+    PageThreeComponent,
+    PageFourComponent,
+    PageFiveComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
