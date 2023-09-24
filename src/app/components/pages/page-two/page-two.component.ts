b15289ea381a9950a30b7687f8e4e0cf8e618495
Matheus Garcia <matheusgarciajbs@gmail.com>
2023-09-24 17:22:45 -0300

    mobile first, animate book

--- src/app/components/pages/page-two/page-two.component.ts
+++ src/app/components/pages/page-two/page-two.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-page-two',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-two.component.css']
 })
 export class PageTwoComponent {
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    // Obtenha a posição da seção em relação à janela
+    const sectionTop = this.el.nativeElement.offsetTop;
+    const sectionBottom = sectionTop + this.el.nativeElement.offsetHeight;
+
+    // Verifique se a seção está visível na tela
+    const isVisible = (window.scrollY >= sectionTop) && (window.scrollY <= sectionBottom);
+
+    // Adicione ou remova a classe 'visible' com base na visibilidade
+    if (isVisible) {
+      this.renderer.addClass(this.el.nativeElement.querySelector('img'), 'visible');
+    } else {
+      this.renderer.removeClass(this.el.nativeElement.querySelector('img'), 'visible');
+    }
+  }
 
 }
